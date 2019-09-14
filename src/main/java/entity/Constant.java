852c8381ee26c48f1e69ec0ad433e5264bb6db08
Jahimees <JerryMous1901@gmail.com>
2019-09-14 09:43:13 +0300

    Realized URL for searching thrends

--- src/main/java/entity/Constant.java
+++ src/main/java/entity/Constant.java
@@ -1,5 +1,12 @@
 package entity;
 
 public abstract class Constant {
-    public static final String URL_CONNECTION = "https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyDCJh3y0HNKissvtz06kG3gEEQvYPF-r98";
+    public static final String MAX_RESULT = "maxResults=10";
+    public static final String YOUTUBE_KEY = "key=AIzaSyDCJh3y0HNKissvtz06kG3gEEQvYPF-r98";
+    public static final String URL_CONNECTION =
+            "https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&"
+                    +MAX_RESULT
+                    +"&chart=mostPopular&regionCode=BY&order=viewCount&"
+                    +YOUTUBE_KEY;
+
 }
